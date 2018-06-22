60ed4017200420416493ec4579241233df082a64
May AlSalem <mayalsalem@Mays-MacBook-Pro.local>
2018-06-23 02:32:17 +0300

    sample commit

---
+++ q1.py
@@ -0,0 +1,8 @@
+user_id=input("Please enter your User ID: (-1 to quit")
+while user_id!=-1:
+        
+     if user_id%2==0:print ("User have been directed to page A")
+     else:print ("User have been directed to page B")
+user_id=input("Please enter your User ID: (-1 to quit")
+
+ 
